9d0f93cbc78829fc0ea4276bad80660fa27b6a9f
Civitaspo <civitaspo@gmail.com>
2017-11-07 09:55:58 +0900

    First Release

---
+++ Casks/sudachi.rb
@@ -0,0 +1,10 @@
+cask 'Sudachi' do
+  version '0.2.1'
+  sha256 '87a3b7b41be0de215fde7ea2816fe9cb613d7bf5'
+
+  url 'https://github.com/kato1628/Sudachi/releases/download/v0.2.1/Sudachi-0.2.1-for-mac-installer.dmg'
+  name 'Sudachi'
+  homepage 'http://sudachi.io/'
+
+  app 'Sudachi.app'
+end
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # homebrew-cask-Sudachi
 Unofficial Homebrew-Cask Installer for Sudachi
+
+# Usage
+
+```
+brew tap civitaspo/homebrew-cask-Sudachi
+brew cask install Sudachi
+```
+
+# ChangeLog
+
+[CHANGELOG.md](./CHANGELOG.md)
