ad2c903ceacb4bda10b42df17184620bf68cdbfd
Civitaspo <civitaspo@gmail.com>
2017-11-07 10:24:50 +0900

    Add a verification comment

--- Casks/sudachi.rb
+++ Casks/sudachi.rb
@@ -2,6 +2,7 @@ cask 'Sudachi' do
   version '0.2.1'
   sha256 'aa1a53d8c5afc87f09c4378d30173f10d2f08aba9f3d747d6518bdd599fef913'
 
+  # github.com/kato1628/Sudachi was verified as official when first introduced to the cask
   url 'https://github.com/kato1628/Sudachi/releases/download/v0.2.1/Sudachi-0.2.1-for-mac-installer.dmg'
   name 'Sudachi'
   homepage 'http://sudachi.io/'
