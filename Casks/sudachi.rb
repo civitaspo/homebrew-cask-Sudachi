b8455f7f2afa26805432c615ee25f2602b09d4fa
Civitaspo <civitaspo@gmail.com>
2017-11-07 10:09:43 +0900

    Fix sha256: shasum -a 256 /path/to/Sudachi-0.2.1-for-mac-installer.dmg

--- Casks/sudachi.rb
+++ Casks/sudachi.rb
@@ -1,6 +1,6 @@
 cask 'Sudachi' do
   version '0.2.1'
-  sha256 '87a3b7b41be0de215fde7ea2816fe9cb613d7bf5'
+  sha256 'aa1a53d8c5afc87f09c4378d30173f10d2f08aba9f3d747d6518bdd599fef913'
 
   url 'https://github.com/kato1628/Sudachi/releases/download/v0.2.1/Sudachi-0.2.1-for-mac-installer.dmg'
   name 'Sudachi'
